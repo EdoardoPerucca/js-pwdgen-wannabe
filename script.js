31fb1e93362daf567bb87573ebed0105391566fe
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-02-21 13:56:30 +0100

    exercise completed

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+*/
+
+const nome = "Edoardo";
+
+prompt("Qual'è il tuo nome")
+
+const cognome = "Perucca";
+
+prompt("Qual'è il tuo cognome")
+
+const colorePreferito = "blu";
+
+prompt("Qual'è il tuo colore preferito?")
+
+document.writeln(nome + cognome + colorePreferito + 1)
+
